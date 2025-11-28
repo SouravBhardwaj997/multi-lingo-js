498ea106ab0579faaac6caf2ac0a3a60d40a89d5
Sourav <souravbhardwaj997@gmail.com>
2025-11-28 13:55:21 +0530

    Some changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+console.log("BASE_URL", BASE_URL);
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
