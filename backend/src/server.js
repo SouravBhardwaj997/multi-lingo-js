1ef2b46c587b65a305c1dae2124a8e926419692d
Sourav <souravbhardwaj997@gmail.com>
2025-11-28 16:30:15 +0530

    trying to expose set-coolie headers

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
+    exposedHeaders: ["set-cookie"],
   })
 );
 
